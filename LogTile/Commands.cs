700d163411b323a2d935aff3b093d73532c469d2
Zack Piispanen <pikakillers@hotmail.com>
2011-12-30 20:27:09 -0500

    Added tshock style command for lookup.

--- LogTile/Commands.cs
+++ LogTile/Commands.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Linq;
 using System.Text;
 using TShockAPI;
@@ -23,13 +22,59 @@ namespace LogTile
 
         public void addHook()
         {
-            ServerHooks.Chat += handleCommand;
+            //ServerHooks.Chat += handleCommand;
+			TShockAPI.Commands.ChatCommands.Add(new Command("lookup", Lookup, "lookup"));
         }
 
         public void closeHook()
         {
-            ServerHooks.Chat -= handleCommand;
+            //ServerHooks.Chat -= handleCommand;
         }
+
+		private void Lookup( CommandArgs args )
+		{
+			TSPlayer ply = args.Player;
+			var param  = args.Parameters;
+			LogTileArgument argument = new LogTileArgument();
+			foreach( string keyval in param)
+			{
+				string[] pair = keyval.Split('=');
+				if (pair.Length < 2)
+					continue;
+				string key = pair[0];
+				string val = pair[1];
+
+				switch( key )
+				{
+					case "area":
+						int radius;
+						int.TryParse(val, out radius);
+						argument.SetRadius( radius );
+						break;
+					case "since":
+						int time;
+						int.TryParse(val, out time);
+						argument.SetSince(time);
+						break;
+					case "page":
+						int page;
+						int.TryParse(val, out page);
+						argument.SetPage( page );
+						break;
+					case "name":
+						argument.SetPlayer(val);
+						break;
+					case "ip":
+						argument.SetIP(val);
+						break;
+					default:
+						break;
+				}
+			}
+			LookupTiles(ply, argument);
+		}
+
+		/*
 
         public void handleCommand(messageBuffer buff, int i, String command, HandledEventArgs args )
         {
@@ -42,11 +87,7 @@ namespace LogTile
                 if (stuff[0] == "/lt")
                 {
                     args.Handled = true;
-                    if (!ply.Group.HasPermission("logtile"))
-                    {
-                        ply.SendMessage( "You do not have permission for that command.", Color.Red);
-                        return;
-                    }
+                    //drop the first item since weve handled it
                     stuff.RemoveAt( 0 );
                     if( stuff.Count == 0 )
                     {
@@ -73,11 +114,13 @@ namespace LogTile
                     }
 
                 }
+                
             }
         }
 
         private void ParseCheck(TSPlayer ply, List<String> args)
         {
+            // toss out the first one since its what dropped us into this.
             args.RemoveAt(0);
             int radius = 10;
             long date = 600;
@@ -119,43 +162,31 @@ namespace LogTile
 
             LookupTiles(ply, radius, date, page, name, ip);
         }
-
-        public long LookupTiles( TSPlayer ply, int radius, long time, int page, string name, string ip )
+		*/
+		private long LookupTiles( TSPlayer ply, LogTileArgument arg )
+		{
+			return LookupTiles(ply, arg.radius, arg.since, arg.page, arg.player, arg.ip);
+		}
+		public long LookupTiles( TSPlayer ply, int radius, long time, int page, string name, string ip )
         {
             var database = TShockAPI.TShock.DB;
-
-            String query = "SELECT * FROM LogTile WHERE X BETWEEN @X1 AND @X2 AND Y BETWEEN @Y1 and @Y2 AND Date > @Date";
-            
-            if (name != "")
-                query += " AND Name = @Name";
-            if (ip != "")
-                query += " AND Name = @IP";
-           
-            query += " ORDER BY id DESC;";
-
+            String query = "SELECT * FROM LogTile WHERE X BETWEEN @0 AND @1 AND Y BETWEEN @2 and @3 AND Date > @4 ORDER BY id DESC;";
             var events = new List<TileEvent>();
-            var reader = QueryReaderDict(database, query, new Dictionary<string, object>
-                                                        {
-                                                            {"X1", ply.TileX - radius},
-                                                            {"X2", ply.TileX + radius},
-                                                            {"Y1", ply.TileY - radius},
-                                                            {"Y2", ply.TileY + radius},
-                                                            {"Date", LogTile.helper.GetTime()-time},
-                                                            {"Name", name},
-                                                            {"IP", ip},
-                                                        });
-
-            while (reader.Read())
+            using (var reader = database.QueryReader(query, ply.TileX - radius, ply.TileX + radius, ply.TileY - radius, 
+                ply.TileY + radius, (LogTile.helper.GetTime()-time) ) )
             {
-                var e = new TileEvent(reader.Get<int>("X"), reader.Get<int>("Y"),
-                                            helper.INTtoString(reader.Get<int>("IP")),
-                                            reader.Get<string>("Name"),
-                                            helper.getAction(reader.Get<int>("Action")),
-                                            reader.Get<int>("TileType"),
-                                            (long)reader.Get<int>("Date"));
-                events.Add(e);
+                while (reader.Read())
+                {
+                    var e = new TileEvent(reader.Get<int>("X"), reader.Get<int>("Y"),
+                                             helper.INTtoString(reader.Get<int>("IP")),
+                                             reader.Get<string>("Name"),
+                                             helper.getAction(reader.Get<int>("Action")),
+                                             reader.Get<int>("TileType"),
+                                             (long)reader.Get<int>("Date"));
+                    events.Add(e);
+                }
             }
-
+            
             if( page == -1 )
             {
                 ply.SendMessage( "There are " + Math.Ceiling( events.Count / 7.0 ) + " pages. (" + events.Count + "edits)");
@@ -178,14 +209,13 @@ namespace LogTile
             Console.WriteLine("Edits made: " + events.Count);
             return events.Count;
         }
-
+		/*
         private void ParseRollback(TSPlayer ply, List<String> args)
         {
+            // toss out the first one since its what dropped us into this.
             args.RemoveAt(0);
             int radius = 10;
             long date = 600;
-            var name = "";
-            var ip = "";
             while (args.Count > 0)
             {
                 String[] s = args[0].Split('=');
@@ -201,57 +231,34 @@ namespace LogTile
                         long.TryParse(val, out date);
                         args.RemoveAt(0);
                         break;
-                    case "name":
-                        name = val;
-                        args.RemoveAt(0);
-                        break;
-                    case "ip":
-                        ip = val;
-                        args.RemoveAt(0);
-                        break;
                     default:
                         args.RemoveAt(0);
                         break;
                 }
             }
-            Console.WriteLine(String.Format("Starting Rollback:\nRadius:{0}\nSince:{1}", radius, date) );
-            var rollback = RollbackTiles(ply, radius, date, name, ip);
-            Console.WriteLine(String.Format("Rollback Complete:\nTiles Rolled Back:{0}", rollback) );
+            Console.WriteLine("Starting Rollback:\nRadius:@0\nSince:@1", radius, date);
+            var rollback = RollbackTiles(ply, radius, date);
+            Console.WriteLine("Rollback Complete:\nTiles Rolled Back:@0", rollback);
         }
 
-        public long RollbackTiles( TSPlayer ply, int radius, long time, string name, string ip )
+        public long RollbackTiles( TSPlayer ply, int radius, long time )
         {
             var database = TShockAPI.TShock.DB;
-            String query = "SELECT * FROM LogTile WHERE X BETWEEN @X1 AND @X2 AND Y BETWEEN @Y1 and @Y2 AND Date > @Date";
-            
-            if (name != "")
-                query += " AND Name = @Name";
-            if (ip != "")
-                query += " AND Name = @IP";
-           
-            query += " ORDER BY id ASC;";
-
+            String query = "SELECT * FROM LogTile WHERE X BETWEEN @0 AND @1 AND Y BETWEEN @2 and @3 AND Date > @4 ORDER BY id ASC;";
             var events = new List<TileEvent>();
-            var reader = QueryReaderDict(database, query, new Dictionary<string, object>
-                                                        {
-                                                            {"X1", ply.TileX - radius},
-                                                            {"X2", ply.TileX + radius},
-                                                            {"Y1", ply.TileY - radius},
-                                                            {"Y2", ply.TileY + radius},
-                                                            {"Date", LogTile.helper.GetTime()-time},
-                                                            {"Name", name},
-                                                            {"IP", ip},
-                                                        });
-
-            while (reader.Read())
+            using (var reader = database.QueryReader(query, ply.TileX - radius, ply.TileX + radius, ply.TileY - radius,
+                ply.TileY + radius, (LogTile.helper.GetTime() - time)))
             {
-                var e = new TileEvent(reader.Get<int>("X"), reader.Get<int>("Y"),
-                                            helper.INTtoString(reader.Get<int>("IP")),
-                                            reader.Get<string>("Name"),
-                                            helper.getAction(reader.Get<int>("Action")),
-                                            reader.Get<int>("TileType"),
-                                            (long)reader.Get<int>("Date"));
-                events.Add(e);
+                while (reader.Read())
+                {
+                    var e = new TileEvent(reader.Get<int>("X"), reader.Get<int>("Y"),
+                                             helper.INTtoString(reader.Get<int>("IP")),
+                                             reader.Get<string>("Name"),
+                                             helper.getAction(reader.Get<int>("Action")),
+                                             reader.Get<int>("TileType"),
+                                             (long)reader.Get<int>("Date"));
+                    events.Add(e);
+                }
             }
 
             List<TileEvent> rollback = new List<TileEvent>();
@@ -279,24 +286,53 @@ namespace LogTile
             }
             return rollback.Count;
         }
-
-        public QueryResult QueryReaderDict(IDbConnection olddb, string query, Dictionary<string, object> values)
-        {
-            var db = olddb.CloneEx();
-            db.Open();
-            using (var com = db.CreateCommand())
-            {
-                com.CommandText = query;
-                foreach (var kv in values)
-                    com.AddParameter("@" + kv.Key, kv.Value);
-
-                return new QueryResult(db, com.ExecuteReader());
-            }
-        }
-
+		*/
         public void save()
         {
             log.saveQueue();
         }
+
+		private class LogTileArgument
+		{
+			public int radius { get; set; }
+			public int since { get; set; }
+			public int page { get; set; }
+			public string player { get; set; }
+			public string ip { get; set; }
+
+			public LogTileArgument()
+			{
+				radius = 10;
+				since = 600;
+				page = 1;
+				player = "";
+				ip = "";
+			}
+
+			public void SetRadius( int r )
+			{
+				radius = r;
+			}
+
+			public void SetSince(int s)
+			{
+				since = s;
+			}
+
+			public void SetPage(int p)
+			{
+				page = p;
+			}
+
+			public void SetPlayer(string p)
+			{
+				player = p;
+			}
+
+			public void SetIP(string i)
+			{
+				ip = i;
+			}
+		}
     }
 }
